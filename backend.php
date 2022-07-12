e66535616d9ed2a789cc45c513a9cf2a5208f4eb
Ryosuke <sessyomaru427@gmail.com>
2022-07-12 22:06:48 +0900

    ログインしていないユーザは管理画面に入れないようにした

--- backend.php
+++ backend.php
@@ -1,3 +1,13 @@
+<?php
+//ログインしているかどうかをセッションを使って判断する
+//セッション開始
+session_start();
+//ログイン時に$_SESSION['id']にユーザIDを入れるため、
+//セッション配列のidというキーに対して値がnullの場合、ログインしていないので、login.phpに遷移させる
+if (!isset($_SESSION['id'])) {
+    header('Location:login.php');
+}
+?>
 <!doctype html>
 <html lang="ja">
 <head>
@@ -6,12 +16,14 @@
     <title>Blog Backend</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <style>
         body {
             padding-top: 5rem;
         }
+
         .bd-placeholder-img {
             font-size: 1.125rem;
             text-anchor: middle;
